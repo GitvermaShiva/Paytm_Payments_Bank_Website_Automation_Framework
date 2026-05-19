12c3e336f7abf5cf678d5cbdc0343faee8cb8125
Shivam Verma <shivamverma63970@gmail.com>
2026-05-19 19:18:57 +0530

    Removed

--- stop-selenium-grid.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Stopping Selenium Grid...
-docker stop selenium-hub selenium-node-chrome selenium-node-firefox
-docker rm selenium-hub selenium-node-chrome selenium-node-firefox
-echo Selenium Grid stopped and removed!
-pause
